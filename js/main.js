1e17e66d9aebe5d4bc756f1009c11b4ad127267a
kny45 <kny.komarov@gmail.com>
2019-10-02 00:12:39 +0300

    Внес изменения по комментариям

--- js/main.js
+++ js/main.js
@@ -1,100 +1,80 @@
 'use strict';
 
-var commentsMap = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
+(function () {
+  var CARD_QUANTITY = 25; // если будет использоваться в других места, то переопределить
 
-var namesMap = [
-  'Иван',
-  'Петр',
-  'Маша',
-  'Семен',
-  'Дарья',
-  'Фродо'
-];
+  var commentsMap = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  ];
 
-var getRandomInt = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+  var namesMap = [
+    'Иван',
+    'Петр',
+    'Маша',
+    'Семен',
+    'Дарья',
+    'Фродо'
+  ];
 
-var generateMocks = function () {
-  var mocks = [];
+  var getRandomInt = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-  for (var i = 0; i < 25; i++) {
+  var generateMocks = function () {
+    var mocks = [];
     var mockComments = [];
-    for (var j = 0; j < getRandomInt(1, 2); j++) {
-      mockComments[j] = {
-        avatar: 'img/avatar-' + getRandomInt(1, 6) + '.svg',
-        comment: commentsMap[getRandomInt(0, 5)],
-        name: namesMap[getRandomInt(0, 5)]
+
+    for (var i = 0; i < CARD_QUANTITY; i++) {
+      mockComments = [];
+      for (var j = 0; j < getRandomInt(1, 2); j++) {
+        mockComments[j] = {
+          avatar: 'img/avatar-' + getRandomInt(1, 6) + '.svg',
+          message: commentsMap[getRandomInt(0, 5)],
+          name: namesMap[getRandomInt(0, 5)]
+        };
+      }
+
+      mocks[i] = {
+        url: 'photos/' + (i + 1) + '.jpg',
+        description: 'photos/' + (i + 1) + '.jpg',
+        likes: getRandomInt(15, 200),
+        comments: mockComments
       };
     }
 
-    var mock = {
-      imgUrl: 'photos/' + (i + 1) + '.jpg',
-      description: 'photos/' + (i + 1) + '.jpg',
-      likes: getRandomInt(15, 200),
-      comments: mockComments
-    };
-    mocks[i] = mock;
-  }
+    return mocks;
+  };
 
-  return mocks;
-};
-
-var createElement = function (template, cardData) {
-  var newElement = template.cloneNode(true);
-  newElement.children[0].src = cardData.imgUrl;
-  newElement.children[0].alt = cardData.description;
-
-  newElement.children[1].children[0].textContent = cardData.comments.length;
-  newElement.children[1].children[1].textContent = cardData.likes;
-
-  return newElement;
-};
-
-var createElements = function () {
-  var cards = generateMocks();
-  var elementsLits = document.querySelector('.pictures');
-  var fragment = document.createDocumentFragment();
-  var template = document.querySelector('#picture').content.querySelector('a');
-
-  for (var i = 0; i < cards.length; i++) {
-    fragment.appendChild(createElement(template, cards[i]));
-  }
-
-  elementsLits.appendChild(fragment);
-};
-
-createElements();
-
-/*
-var createElements = function () {
-  var cards = generateMocks();
-  var elementsLits = document.querySelector('.pictures');
-  var fragment = document.createDocumentFragment();
-  var template = document.querySelector('#picture').content.querySelector('a');
-
-  for (var i = 0; i < cards.length; i++) {
+  var createElement = function (template, cardData) {
     var newElement = template.cloneNode(true);
-    newElement.children[0].src = cards[i].imgUrl;
-    newElement.children[0].alt = cards[i].description;
+    newElement.querySelector('.picture__img').src = cardData.url;
+    newElement.querySelector('.picture__img').alt = cardData.description;
 
-    newElement.children[1].children[0].textContent = cards[i].comments.length;
-    newElement.children[1].children[1].textContent = cards[i].likes;
+    newElement.querySelector('.picture__comments').textContent = cardData.comments.length;
+    newElement.querySelector('.picture__likes').textContent = cardData.likes;
 
-    fragment.appendChild(newElement);
-  }
+    return newElement;
+  };
 
-  elementsLits.appendChild(fragment);
-};
-*/
+  var createElements = function () {
+    var cards = generateMocks();
+    var elementsLits = document.querySelector('.pictures');
+    var fragment = document.createDocumentFragment();
+    var template = document.querySelector('#picture').content.querySelector('a');
 
-// console.log(generateMocks());
+    for (var i = 0; i < cards.length; i++) {
+      fragment.appendChild(createElement(template, cards[i]));
+    }
+
+    elementsLits.appendChild(fragment);
+  };
+
+  createElements();
+})();
